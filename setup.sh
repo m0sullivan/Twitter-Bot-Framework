869670b04f5bdc86c50c73ee8e404fa40f952bca
yoobadooba <peakinsanity@tutanota.com>
2023-08-19 20:44:00 -0400

    slight change

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+mkdir logs
+mkdir configs
+mkdir media
+mkdir tweetdeck_configs
+mkdir tweetdeck_userids
 docker build -t twitter .
 docker run -it --name twitcontainer -d -p 42874:42874 twitter
